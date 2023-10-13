62743515177803c481980e3952621d450f677cd1
pripriya09 <pripriya0904@gmail.com>
2023-10-13 19:01:42 -0400

    done

---
+++ script.js
@@ -0,0 +1,57 @@
+
+const taskValue = document.getElementsByClassName("task_value")[0];
+const taskSubmit = document.getElementsByClassName("task_submit")[0];
+const taskList = document.getElementsByClassName("task_list")[0];
+
+ const addTasks = (task) => {
+    
+        const listItem = document.createElement('li');
+        const showItem = taskList.appendChild(listItem);
+        showItem.innerHTML = task;
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = 'Delete';
+        listItem.appendChild(deleteButton);
+        const edit = document.createElement('button');
+        edit.innerHTML = 'edit';
+        listItem.appendChild(edit);
+    
+        
+        deleteButton.addEventListener('click', evt => {
+            evt.preventDefault();
+            deleteTasks(deleteButton);
+        });
+    };
+    const deleteTasks = (deleteButton) => {
+        const chosenTask = deleteButton.closest('li');
+        taskList.removeChild(chosenTask);
+    };
+    taskSubmit.addEventListener('click', evt => {
+        evt.preventDefault();
+        const task = taskValue.value;
+        addTasks(task);
+        taskValue .value = '';
+    });
+
+
+
+
+
+
+
+
+
+
+
+// form.addEventListener("submit",(e)=>{
+//     e.preventDefault();
+//     const task = input.ValueMax;
+
+//     if(!task){
+//         alert("plz fill out task")
+//         return;
+//     }
+//       const task_el=document.createElement("div")
+//       tasl_el.c
+
+// })
+
